054ac6150b5898809baccff7a3a203a46b424938
1156859110 <1156859110@qq.com>
2018-11-24 11:20:40 +0800

    添加守护进程，防止ssh断线之后程序停止运行

--- httpd/http_server.cc
+++ httpd/http_server.cc
@@ -3,11 +3,15 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
-#include	<sys/types.h>
+#include <sys/types.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <string.h>
 #include <cstdlib>
+#include<fcntl.h>
+#include<sys/stat.h> 
+#include<signal.h>
+#include <sys/resource.h>
 using namespace std;
 
 const int LISTENQ = 1024;
@@ -38,10 +42,10 @@ int BindListen(int port)
 string  pages = "<html><head>\
 	<title>top news</title>\
 	</head><body>\
-	<p>Welcome to husky's top news.<br />\
-	</p>\
-	<hr>\
-	</body></html>" ;
+	<div align=\"center\">\
+		Welcome to husky's home page！\
+	</div>\
+	</body></html>";
 void HomePage(int fd) 
 {
 	string reply;     
@@ -61,7 +65,61 @@ void process(int fd)
 	read(fd,buf,9600);
 	HomePage(fd);
 }
-int main() 
+typedef void (*pf)(); 
+
+void
+daemonize(pf pFunction)
+{
+	int					i, fd0, fd1, fd2;
+	pid_t				pid;
+	struct rlimit		rl;
+	struct sigaction	sa;
+
+	umask(0);
+	/*获取最大文件描述*/
+	if (getrlimit(RLIMIT_NOFILE, &rl) < 0)
+		cout<<"can't get file limit"<<endl;
+	/*失去控制终端*/
+	if ((pid = fork()) < 0)
+		cout<<"fork error"<<endl;
+	else if (pid != 0) 
+		exit(0);
+	setsid();
+	/*禁止打开终端，同时忽略sighup信号*/
+	sa.sa_handler = SIG_IGN;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	if (sigaction(SIGHUP, &sa, NULL) < 0)
+		cout<<"can't ignore SIGHUP"<<endl;
+	if ((pid = fork()) < 0)
+		cout<<"fork error "<<endl;
+	else if (pid != 0) 
+		exit(0);
+
+	if (chdir("/") < 0)
+		cout<<"can't change directory to /"<<endl;
+	/*关闭打开的文件描述符*/
+	if (rl.rlim_max == RLIM_INFINITY)
+		rl.rlim_max = 1024;
+	for (i = 0; i < rl.rlim_max; i++)
+		close(i);
+	/*将标准输入输出重定向到/dev/null.*/
+	fd0 = open("/dev/null", O_RDWR);
+	fd1 = dup(0);
+	fd2 = dup(0);
+#if 0
+	/* * Initialize the log file. */
+	openlog(cmd, LOG_CONS, LOG_DAEMON);
+	if (fd0 != 0 || fd1 != 1 || fd2 != 2) {
+		syslog(LOG_ERR, "unexpected file descriptors %d %d %d",
+		  fd0, fd1, fd2);
+		exit(1);
+	}
+#endif
+	
+	pFunction();
+}
+void server() 
 {
 	int listenfd, connfd;
 	int clientlen;
@@ -74,4 +132,8 @@ int main()
 		process(connfd);                                             
 		close(connfd);                                           
 	}
+}
+int main() 
+{
+	daemonize(server);
 }
