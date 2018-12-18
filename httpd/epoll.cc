4118da9f24b9e0cf88dc16716c2b370bf14e84bd
1156859110 <1156859110@qq.com>
2018-12-18 23:24:23 +0800

    添加epoll具体事件修改

--- httpd/epoll.cc
+++ httpd/epoll.cc
@@ -1,25 +1,57 @@
 #include "common.h"
 #include "log.h"
-//const int EPSIZE = 4096；
-//ectltype = EPOLL_CTL_DEL,EPOLL_CTL_ADD;
-//etype = EPOLLIN
-void Epoll::epollCtl(int fd,int events,int ctltype) 
-{
-	struct epoll_event ev;
-	ev.data.fd = fd;
-	ev.events = events;
-	if(epoll_ctl(epfd,ctltype,fd,&ev) < 0){
-		 std::cout<<"error"<<endl;
-	}            
+
+Epoll(int pfd):epfd(epoll_create(1),rpipe(pfd){
+	addInEvents(rpipe);
 }
-vector<int> Epoll::epollWait() 
+~Epoll(){
+	delInEvents(rpipe);
+}
+vector<struct epoll_event> Epoll::epollWait() 
 {
-	fdsize = epoll_wait(epfd,&vfd[0],vfd.size(),-1);
-	vector<int>v(vfd,vfd+fdsize);
+	int num = epoll_wait(epfd,&evts[0],evts.size(),-1);
+	vector<struct epoll_event>v(evts,evts+num);
 	std::cout<<fdactive<<" active events"<<endl; 
 	LOG_DEBUG<<fdactive<<" active events"; 
 	return v; 
 }
-int Epoll::getFdSize() {
-	return fdsize; 
+int Epoll::addInEvents(int fd){
+	int rc = 0;
+	struct epoll_event ev;
+	ev.data.fd = fd;
+	ev.events = EPOLLIN;
+	if((rc = epoll_ctl(epfd,EPOLL_CTL_ADD,fd,&ev)) < 0){
+		 LOG_ERROR<<"add in epoll error"
+	}     
+	return rc;       
+}
+int Epoll::delInEvents(int fd){
+	int rc = 0;
+	struct epoll_event ev;
+	ev.data.fd = fd;
+	ev.events = EPOLLIN;
+	if((rc = epoll_ctl(epfd,EPOLL_CTL_DEL,fd,&ev)) < 0){
+		 LOG_ERROR<<"epoll in del error"
+	}     
+	return rc;     
+}
+int Epoll::addOutEvents(int fd){
+	int rc = 0;
+	struct epoll_event ev;
+	ev.data.fd = fd;
+	ev.events = EPOLLIN |  EPOLLOUT;
+	if((rc = epoll_ctl(epfd,EPOLL_CTL_MOD,fd,&ev)) < 0){
+		 LOG_ERROR<<"add out epoll error"
+	}     
+	return rc;       
+}
+int Epoll::delOutEvents(int fd){
+	int rc = 0;
+	struct epoll_event ev;
+	ev.data.fd = fd;
+	ev.events = EPOLLIN;
+	if((rc = epoll_ctl(epfd,EPOLL_CTL_MOD,fd,&ev)) < 0){
+		 LOG_ERROR<<"del out events error"
+	}     
+	return rc;       
 }
--- httpd/epoll.h
+++ httpd/epoll.h
@@ -4,19 +4,19 @@
 class Epoll{
 private:
 	int epfd;
-	int pipefd;
-	int fdsize;
-	vector<int>vfd;
+	int rpipe;
+	vector<struct epoll_event>evts;
 public:
-	Epoll(int fd):epfd(epoll_create(1),pipefd(fd){
-		epollCtl(pipefd,EPOLLIN,EPOLL_CTL_ADD);
-	}
-	Epoll():epfd(epoll_create(1){};
-	~Epoll(){
-		epollCtl(pipefd,EPOLLIN,EPOLL_CTL_DEL);
-		close(pipefd);
-	}
-	void epollCtl(int fd,int events,int ctltype) {};
-	void epollWait();
-	int getFdSize();
+	Epoll(int pfd);
+	~Epoll();
+	vector<struct epoll_event> epollWait() ;
+	
+	int addInEvents(int fd);
+	
+	int delInEvents(int fd);
+	
+	int addOutEvents(int fd);
+	
+	int delOutEvents(int fd);
+	
 }
