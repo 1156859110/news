0141b139128f6eb3193ff9df959faa2267084501
1156859110 <1156859110@qq.com>
2018-12-19 19:04:24 +0800

    线程池完善通知机制

--- httpd/threadPoll.cc
+++ httpd/threadPoll.cc
@@ -2,25 +2,41 @@
 #include "common.h"
 #include "threadPoll.h"
 
-void ThreadPoll::create()
-{
+	
+ThreadPoll::ThreadPool():
+threadnum(get_nprocs()*1.5),bdestroy(false),tid(threadnum,0),
+pevtthrd(new[threadnum]EventThread()),count(0){
 	for(int i = 0; i < threadnum; ++i){
-		pthread_create(&threadId[i], NULL, pevent[i]->run, NULL);
+		pthread_create(&tid[i], NULL, pevtthrd[i]->run, NULL);
+	}		
+}
+ThreadPoll::ThreadPool(int tnum):
+threadnum(tnum),bdestroy(false),tid(threadnum,0),
+pevtthrd(new[threadnum]EventThread()),count(0){
+	for(int i = 0; i < threadnum; ++i){
+		pthread_create(&tid[i], NULL, pevtthrd[i]->run, NULL);
 	}	
 }
-void ThreadPoll::destroy()
-{
-	if(bdestroy)return;
-	destroy = true;
+void ThreadPoll::~ThreadPool(){
 	for(int i = 0; i < threadnum; ++i){
-		pevent[tid]->destroy();
-		//需要等待线程退出吗？
-		pthread_join(threadId[i], NULL);
-		cout<<"thread "<< pthread_self()<<endl;
+		pevtthrd[i]->destroy();
+		pthread_join(tid[i], NULL);
+		LOG_DEBUG<<"thread destroy";
+		cout<<"thread destroy"<< pthread_self()<<endl;
 	}
+	delete[] pevtthrd;
 }
+int ThreadPoll::getThreadNum(){
+	return threadnum;
+}
+void ThreadPoll::notify(){
+	for(int i = 0; i < threadnum; ++i){
+		pevtthrd[i]->notifyThread();
+	}
+};
+
 EventThread* ThreadPoll::getEventThread(){
-	unsigned int tid = count%threadnum;
+	unsigned int id = count%threadnum;
 	++count;
-	return pevent[tid];
+	return pevtthrd[id];
 }
--- httpd/threadPoll.h
+++ httpd/threadPoll.h
@@ -1,20 +1,21 @@
 #include "common.h"
-typedef void (*pFun) (int arg);
 
+class EventThread;
 class ThreadPoll
 {
 private:
 	bool bdestroy;
 	int threadnum;
-	EventThread *pevent;
-	std::vector<pthread_t>threadId;
+	EventThread *pevtthrd;
+	std::vector<pthread_t>tid;
 	unsigned int count ;
 public:
-	
-	ThreadPool():threadnum(get_nprocs()*1.5),bdestroy(false),threadId(threadnum,0){};
-	ThreadPool(int tnum):threadnum(tnum),bdestroy(false),threadId(threadnum,0),pevent(new[threadnum]EventThread()){};
-	void PoolDestroy();
-
+	ThreadPool();
+	ThreadPool(int tnum);
+	~ThreadPool();
+	int getThreadNum();
+	void notify();
+	EventThread* getEventThread();
 };
 
 
