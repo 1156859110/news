082d7dc3d36c322a30fc527deb5bf6e23b9b1b0d
1156859110 <1156859110@qq.com>
2019-01-09 23:24:33 +0800

    添加新的数据来源以及数据库处理

--- spider/spider.py
+++ spider/spider.py
@@ -3,13 +3,21 @@
 #抓取的新闻网站：
 #联合早报：需要屏蔽掉订户https://www.zaobao.com.sg/znews/greater-china
 #bbc中文网 https://www.bbc.com/zhongwen/simp/chinese_news
-
+ #service mysqld stop
+ #service mysqld start
+ #show tables;
+ #CREATE DATABASE news; 
+ #use news;
+ #CREATE TABLE newstable (id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,title TINYTEXT,bimg tinyint(1) ,pubdate VARCHAR(10),url VARCHAR(100),article TEXT) ENGINE=InnoDB,default character set utf8;
+#每隔30分钟执行一次
+# */30 * * * * python /root/news/spider.py
 import newspaper
 import re
 import pymysql
 import os
 import urllib 
 import requests
+import datetime
 from newspaper import Article
 
 def connectDatabase():
@@ -28,53 +36,81 @@ def checkUrl(cur,url):
     cur.execute("SELECT id FROM newstable WHERE url = %s" ,url)
     row = cur.fetchone();
     return row
+    
 
-def writeDatabase(cur, url):
+def writeDatabase(cur, url,filter):
         article = Article(url,language = 'zh')
         article.download()
         article.parse()
-        print(article.title)
+        #print(article.title)
         print("\n")
-        print(article.top_image)
+        #print(article.top_image)
         print("\n")
-        print(len(article.text))
-        print("\n")
-        print(article.publish_date)
+        #print(len(article.text))
         print("\n")
          
         #需要去除短小无效内容以及重复的url,
-        #下载图片并重新命名为
-        if len(article.text) < 500 or checkUrl(cur,url):
+        if filter and len(article.text) < 500:
+            return
+        elif checkUrl(cur,url):
             return
         else:
-            cur.execute(
-           """INSERT INTO  newstable(title,article, url)
-              VALUES (%s, %s, %s)""",
-              (article.title, article.text,article.url))
-            image = checkUrl(cur,url)
-            #print(image)
-            #print("\n")
-            uploadServer(article.top_image,image[0])
+            pubdate = datetime.date.today()
+            if(article.top_image.find('.jpg') == -1):
+                bimg = 0
+                cur.execute("""INSERT INTO  newstable(title,bimg,pubdate, url,article)VALUES (%s, %s, %s,%s, %s)""",
+                (article.title,bimg,pubdate,article.url,article.text))
+            else:
+                bimg = 1
+                cur.execute(
+                """INSERT INTO  newstable(title,bimg,pubdate, url,article)
+                VALUES (%s, %s,%s, %s, %s)""",
+                (article.title,bimg,pubdate,article.url,article.text))
+                #下载图片并重新命名为
+                image = checkUrl(cur,url)
+                #print(image)
+                #print("\n")
+                uploadServer(article.top_image,image[0])
+              
+            
+            
 def bbcSpider(cur):
     allurls = newspaper.build('https://www.bbc.com/zhongwen/simp/chinese_news')
     bbc = re.findall(r"(?<=href=\")\/zhongwen\/simp\/chinese-news-.*?(?=\")", allurls.html)
     #需要保证逆序列，保证最新的在在最后
     for link in bbc[::-1]:
         #print("https://www.bbc.com"+link)
-        writeDatabase(cur,"https://www.bbc.com"+link)   
+        writeDatabase(cur,"https://www.bbc.com"+link,1)   
+        
+        
+        
           
 def zaobaoSpider(cur):
     allurls = newspaper.build('https://www.zaobao.com.sg/znews/greater-china')
     zaobao = re.findall(r"(?<=href=\")\/znews\/greater-china\/.*?(?=\")", allurls.html)
     for link in zaobao[::-1]:
         #print("https://www.zaobao.com.sg"+link)
-        writeDatabase(cur, "https://www.zaobao.com.sg"+link)  
+        writeDatabase(cur, "https://www.zaobao.com.sg"+link,1)  
+        
+def realtimeSpider(cur):
+    allurls = newspaper.build('https://www.zaobao.com.sg/realtime/china')
+    zaobao  = re.findall(r"(?<=href=\")\/realtime\/china\/.*?(?=\")", allurls.html)
+    for link in zaobao[::-1]:
+        #print("https://www.zaobao.com.sg"+link)
+        writeDatabase(cur, "https://www.zaobao.com.sg"+link,0)      
+        
+        
+        
 def uploadServer(url,image):
-        os.chdir('/root/news') #打开要保存的文件夹
+        os.chdir('/root/news/image') #打开要保存的文件夹
         urllib.request.urlretrieve(url,'%s.jpg' %image)
-        os.system('scp  '+str(image) +'.jpg root@47.92.96.91:/root/news')
+        os.system('scp  '+str(image) +'.jpg root@47.92.96.91:/root/news/image')
+        
+        
+        
 def main():
     cur = connectDatabase()
+    realtimeSpider(cur)
     zaobaoSpider(cur)
     bbcSpider(cur)
     closDatabase(cur)
