1a1427081910387bf6a2c960aecaae5ce4d11c20
1156859110 <1156859110@qq.com>
2019-02-09 15:00:40 +0800

    降序排列数据

--- httpd/mysqlDb.cc
+++ httpd/mysqlDb.cc
@@ -6,11 +6,22 @@ const char USER[] = "root" ;
 const char PWD[] = "news/123" ;
 const char DBNAME[] = "news" ;
 const int PORT = 3306;
-MysqlDb::MysqlDb():mysql(NULL),row(NULL),result(NULL),key(0){
+
+OrmTable::OrmTable():sid(""),artlen(0),imglen(0),cnt(0),stitle(""),bimg(false),spubdate(""){
+	//todo 待修改
+	particle = NULL;
+	pimg = NULL;
+}
+OrmTable::~OrmTable(){
+	//particle = NULL;
+	//pimg = NULL;
+}
+MysqlDb::MysqlDb():mysql(NULL),row(NULL),result(NULL),skey("0"){
   mysql = mysql_init(NULL);
   if(mysql == NULL) std::cout << "Error: " << mysql_error(mysql);   
+  
 }
-MysqlDb::MysqlDb(int curid):mysql(NULL),row(NULL),result(NULL),key(curid){
+MysqlDb::MysqlDb(std::string sid):mysql(NULL),row(NULL),result(NULL),skey(sid){
   mysql = mysql_init(NULL);
   if(mysql == NULL) std::cout << "Error: " << mysql_error(mysql);   
 }
@@ -27,12 +38,15 @@ bool MysqlDb::init(){
 		std::cout<< "connect error " << mysql_error(mysql);
 		return false;
 	}
+    if (mysql_set_character_set(mysql, "utf8" ) ) { 
+    	std::cout<< "set utf8 error "<<mysql_error(mysql); 
+    } 
 	return true;
 }
  
 std::string MysqlDb::queryArticle(){
 	std::string sql("SELECT article FROM newstable WHERE id = ");
-	sql += std::to_string (key);
+	sql += skey;
 	//返回0表示成功。
 	if(mysql_query(mysql,sql.c_str())){
 		std::cout << "query fail: " << mysql_error(mysql)<<std::endl;  
@@ -49,35 +63,39 @@ std::string MysqlDb::queryArticle(){
 		return str;
 	}
 }
-std::unordered_map<std::string, Sdbtable> MysqlDb::queryTitle()
+int MysqlDb::queryTitle(std::unordered_map<std::string, OrmTable> &dbmap)
 {
-	std::unordered_map<std::string, Sdbtable>dbmap;
 	std::string sql("SELECT id,title,bimg,pubdate FROM newstable WHERE id > ");
-	sql += std::to_string (key);
+	sql += skey;
+	sql += " order by id desc ";
 	
 	if(mysql_query(mysql,sql.c_str())){
 		std::cout << "query fail: " << mysql_error(mysql);   
-		return dbmap;                                          
+		return -1;                                          
 	}
 	else{
-		Sdbtable sdb;
-		
+		bool bid = true;
+		std::string cursid;
 		result = mysql_store_result(mysql);
 		//int colnum = mysql_num_fields(result);
 		while ((row = mysql_fetch_row (result))){
+			//unsigned long *rowlen = mysql_fetch_lengths(result);
+			if(bid){
+				cursid += row[0];
+				bid = false;
+			}
+			OrmTable otb;
 			std::cout<<row[0]<<" "<<row[1]<<" "<<row[2]<<" "<<row[3]<<" "<<std::endl;
-			memset(&sdb,0,sizeof(sdb));
-			sdb.id = atoi(row[0]);
-			strncpy(sdb.title,row[1],49);
-			sdb.title[49] = '\0';
-			sdb.bimg = row[2];
-			strncpy(sdb.pubdate,row[3],9);
-			sdb.pubdate[9] = '\0';
-			sdb.particle = NULL;
-			sdb.pimg = NULL;
-			dbmap[std::to_string(sdb.id)] = sdb;
+			otb.sid += row[0];
+			otb.stitle += row[1];
+			otb.bimg = row[2];
+			otb.spubdate += row[3];
+			otb.particle = NULL;
+			otb.pimg = NULL;
+			dbmap[otb.sid] = otb;
 		}
 		mysql_free_result(result);
-		return dbmap;
+		return bid?0:atoi(cursid.c_str());
 	}
 }
+
